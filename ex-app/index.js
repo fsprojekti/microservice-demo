33b0b3937efab45744d77477ba034da1dc0d75fa
Nejc Kruh <kruh.nejc@gmail.com>
2023-01-07 12:28:27 +0100

    index.js: added static file serving

--- ex-app/index.js
+++ ex-app/index.js
@@ -10,6 +10,9 @@ app.use(bodyParser.json());
 meritveRouter(app)
 krmilnikRouter(app)
 
+// static
+app.use('/static', express.static('database'))
+
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 });
